a89a7730e651361ff3ef2441ccd65b59a43dad22
ojimac <ojimac@gmail.com>
2010-09-05 11:26:38 +0900

    add-to-load-path関数の間違い修正

--- init.el
+++ init.el
@@ -1,23 +1,23 @@
 ;; load-pathを追加する関数を定義
-;; (defun add-to-load-path (&rest paths)
-;;   (let (path)
-;;     (dolist (path paths paths)
-;;       (let ((default-directory (expand-file-name (concat user-emacs-directory path)))
-;;  	    (add-to-list 'load-path default-directory)
-;;  	    (if (fboundup 'normal-top-level-add-subdirs-to-load-path)
-;;  		(normal-top-level-add-subdirs-to-load-path)))))))
+(defun add-to-load-path (&rest paths)
+  (let (path)
+    (dolist (path paths paths)
+      (let ((default-directory (expand-file-name (concat user-emacs-directory path))))
+  	    (add-to-list 'load-path default-directory)
+  	    (if (fboundp 'normal-top-level-add-subdirs-to-load-path)
+  		(normal-top-level-add-subdirs-to-load-path))))))
 
 ;; elispとconfディレクトリをサブディレクトリごとload-pathに追加
-;;(add-to-load-path "elisp" "conf")
+(add-to-load-path "elisp" "conf")
 
 ;; (install-elisp "http://www.emacswiki.org/emacs/download/auto-install.el")
-;;(when (require 'auto-install nil t)
+(when (require 'auto-install nil t)
   ;; インストールディレクトリを設定する 初期値は ~/.emacs.d/auto-install/
-;;  (setq auto-install-directory "~/.emacs.d/elisp/")
+  (setq auto-install-directory "~/.emacs.d/elisp/")
   ;; EmacsWikiに登録されているelispの名前を取得する
-;;  (auto-install-update-emacswiki-package-name t)
+  (auto-install-update-emacswiki-package-name t)
   ;; install-elispの関数を利用可能にする
-;;  (auto-install-compatibility-setup))
+  (auto-install-compatibility-setup))
 
 ;;起動時のメッセージを消す
 (setq inhibit-startup-message t)
